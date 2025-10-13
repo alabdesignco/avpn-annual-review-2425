2ea1987c7282668317a818ce6e700e845ab7a3bd
hiraya-dev <neilr.manalo@gmail.com>
2025-10-13 12:31:41 +0800

    Update build

--- scripts/animations/infiniteCanvas.js
+++ scripts/animations/infiniteCanvas.js
@@ -9,6 +9,8 @@ class Grid {
     this.dom = document.querySelector(".section_home-header")
     this.grid = document.querySelector(".home-header_wrapper")
     this.products = [...document.querySelectorAll(".home-header_image-wrapper")]
+    this.heading = document.querySelector(".home-header_heading")
+    this.navbar = document.querySelector(".navbar_component")
 
     this.isDragging = false
   }
@@ -19,6 +21,7 @@ class Grid {
 
   intro() {
     this.centerGrid()
+    this.setupSplitText()
 
     const timeline = gsap.timeline()
 
@@ -26,6 +29,18 @@ class Grid {
     timeline.set(this.products, {
       scale: 0.5,
       opacity: 0,
+    })
+    timeline.set(this.navbar, {
+      y: -100,
+      autoAlpha: 0
+    })
+
+    timeline.from(this.headingChars, {
+      yPercent: 100,
+      autoAlpha: 0,
+      duration: 2,
+      ease: "expo.inOut",
+      stagger: 0.02
     })
 
     timeline.to(this.products, {
@@ -41,13 +56,29 @@ class Grid {
     timeline.to(this.dom, {
       scale: 1,
       duration: 1.2,
-      ease: "power3.inOut",
-      onComplete: () => {
-        this.setupDraggable()
-        this.addEvents()
-        this.observeProducts()
-      }
+      ease: "power3.inOut"
     })
+    timeline.to(this.navbar, {
+      y: 0,
+      autoAlpha: 1,
+      duration: 0.8,
+      ease: "power3.out"
+    }, "-=0.4")
+    timeline.call(() => {
+      this.setupDraggable()
+      this.addEvents()
+      this.observeProducts()
+    })
+  }
+
+  setupSplitText() {
+    if (!this.heading) return
+
+    this.splitText = SplitText.create(this.heading, { 
+      type: "chars",
+      mask: "chars"
+    })
+    this.headingChars = this.splitText.chars
   }
 
   centerGrid() {
