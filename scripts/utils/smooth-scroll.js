92f5d04d733c191ed03fd23aa40dd232eedd9cd2
hiraya-dev <neilr.manalo@gmail.com>
2025-11-03 23:24:05 +0800

    Fix iOS snap-back on fast scroll - Add syncTouch and smoothTouch to Lenis config

--- scripts/utils/smooth-scroll.js
+++ scripts/utils/smooth-scroll.js
@@ -1,5 +1,16 @@
 const smoothScroll = () => {
-  const lenis = new Lenis();
+  const lenis = new Lenis({
+    duration: 1.1,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    orientation: 'vertical',
+    gestureOrientation: 'vertical',
+    smoothWheel: true,
+    wheelMultiplier: 1,
+    smoothTouch: true,
+    syncTouch: true,
+    touchMultiplier: 1.5,
+    infinite: false
+  });
 
   lenis.on('scroll', ScrollTrigger.update);
 
