a275c96459c592e84c0cc3d0f330fffc0169f06d
hiraya-dev <neilr.manalo@gmail.com>
2025-11-04 08:45:00 +0800

    Fix selector syntax: use :scope > * for direct children in mobile animation

--- scripts/animations/sections/impact-action.js
+++ scripts/animations/sections/impact-action.js
@@ -13,7 +13,8 @@ export const initImpactActionSection = () => {
       const circle = item.querySelector('.highlight_timeline-circle');
       
       if (isMobile) {
-        gsap.set(item, { autoAlpha: 0, x: 100 });
+        const children = item.querySelectorAll(':scope > *');
+        gsap.set(children, { autoAlpha: 0, x: 100 });
       } else {
         gsap.set(item, { autoAlpha: 0, y: 30 });
       }
@@ -46,36 +47,62 @@ export const initImpactActionSection = () => {
     
     contentItems.forEach((item, index) => {
       const circle = item.querySelector('.highlight_timeline-circle');
-      const animProps = isMobile 
-        ? { autoAlpha: 1, x: 0, duration: 0.6, ease: "power2.out", delay: index * 0.1 }
-        : { autoAlpha: 1, y: 0, duration: 0.6, ease: "power2.out" };
       
-      ScrollTrigger.create({
-        trigger: item,
-        start: "top center",
-        toggleActions: "play none none reverse",
-        animation: gsap.to(item, animProps),
-        onEnter: () => {
-          if (circle) {
-            circle.classList.add('is-active');
+      if (isMobile) {
+        const children = item.querySelectorAll(':scope > *');
+        const tl = gsap.timeline({ paused: true });
+        
+        tl.to(children, {
+          autoAlpha: 1,
+          x: 0,
+          duration: 0.5,
+          ease: "power2.out",
+          stagger: 0.1
+        });
+        
+        ScrollTrigger.create({
+          trigger: item,
+          start: "top center",
+          toggleActions: "play none none reverse",
+          animation: tl,
+          onEnter: () => {
+            if (circle) circle.classList.add('is-active');
+          },
+          onEnterBack: () => {
+            if (circle) circle.classList.add('is-active');
+          },
+          onLeave: () => {
+            if (circle) circle.classList.remove('is-active');
+          },
+          onLeaveBack: () => {
+            if (circle) circle.classList.remove('is-active');
           }
-        },
-        onEnterBack: () => {
-          if (circle) {
-            circle.classList.add('is-active');
+        });
+      } else {
+        ScrollTrigger.create({
+          trigger: item,
+          start: "top center",
+          toggleActions: "play none none reverse",
+          animation: gsap.to(item, { 
+            autoAlpha: 1, 
+            y: 0, 
+            duration: 0.6, 
+            ease: "power2.out" 
+          }),
+          onEnter: () => {
+            if (circle) circle.classList.add('is-active');
+          },
+          onEnterBack: () => {
+            if (circle) circle.classList.add('is-active');
+          },
+          onLeave: () => {
+            if (circle) circle.classList.remove('is-active');
+          },
+          onLeaveBack: () => {
+            if (circle) circle.classList.remove('is-active');
           }
-        },
-        onLeave: () => {
-          if (circle) {
-            circle.classList.remove('is-active');
-          }
-        },
-        onLeaveBack: () => {
-          if (circle) {
-            circle.classList.remove('is-active');
-          }
-        }
-      });
+        });
+      }
     });
   });
 }
